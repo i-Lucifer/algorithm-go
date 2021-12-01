03b2388d6a89884a6f6c2cfb6d94bd8481ca989b
Lucifer <82418@163.com>
2021-12-01 18:50:42 +0800

    arrary_2_tree

--- tree/array_2_tree_test.go
+++ tree/array_2_tree_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestArray2Tree(t *testing.T) {
+func TestToTree(t *testing.T) {
 	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	fmt.Println("array: ", data)
 	root := Array2Tree(data) // 二分法转为树后，6 3,9 2,5,8,10 1,4,7
