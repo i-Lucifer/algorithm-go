b21548103b818ac875131e14cb9e1e014d97d108
Lucifer <82418@163.com>
2021-12-01 21:41:34 +0800

    层次遍历

--- tree/breadth_first_layer.go
+++ tree/breadth_first_layer.go
@@ -7,15 +7,15 @@ import (
 // 广度优先
 // 层次遍历
 // 二叉树的层次遍历一般需要借助队列
-func LayerOrder(root *BNode) {
-	if root == nil {
+func LayerOrder(node *BNode) {
+	if node == nil {
 		return
 	}
 	var queue []*BNode
-	queue = append(queue, root)
+	queue = append(queue, node)
 
 	for len(queue) > 0 {
-		node := queue[0]
+		node, queue = pull(queue)
 		// for _, node := range queue { // range不能感知queue的变化，如果queue遍历完不空，再遍历一次
 		fmt.Println(node.Data)
 		if node.LChild != nil {
@@ -24,20 +24,21 @@ func LayerOrder(root *BNode) {
 		if node.RChild != nil {
 			queue = append(queue, node.RChild)
 		}
-		queue = queue[1:len(queue)]
+		// queue = queue[1:len(queue)]
 		// }
 	}
 }
 
-func LayerOrder2(root *BNode) {
-	if root == nil {
+// 缺点，队列的值没有被取出，空间复杂度为O(n)
+func LayerOrder2(node *BNode) {
+	if node == nil {
 		return
 	}
 	var queue []*BNode
-	queue = append(queue, root)
+	queue = append(queue, node)
 
 	for i := 0; i < len(queue); i++ {
-		node := queue[i]
+		node := queue[i] // 遍历取值
 		fmt.Println(node.Data)
 		if node.LChild != nil {
 			queue = append(queue, node.LChild)
@@ -45,6 +46,5 @@ func LayerOrder2(root *BNode) {
 		if node.RChild != nil {
 			queue = append(queue, node.RChild)
 		}
-		// queue = queue[1:len(queue)] // for i 不需要对queue进行截取，也就是queue会增大，时间复杂度和空间复杂度都是O(n)
 	}
 }
