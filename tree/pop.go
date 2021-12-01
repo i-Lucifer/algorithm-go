4ff514302a93643bb9a69cb8ce1fa12b614f90e1
Lucifer <82418@163.com>
2021-12-01 21:40:40 +0800

    封装出队

--- tree/pop.go
+++ tree/pop.go
@@ -1,8 +1,16 @@
 package tree
 
+// 出栈
 func pop(stack []*BNode) (*BNode, []*BNode) {
 	lenth := len(stack)
 	node := stack[lenth-1]
 	stack = stack[0 : lenth-1]
 	return node, stack
+}
+
+// 出队
+func pull(queue []*BNode) (*BNode, []*BNode) {
+	node := queue[0]
+	queue = queue[1:len(queue)]
+	return node, queue
 }
